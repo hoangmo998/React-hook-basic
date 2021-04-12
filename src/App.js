bdc6c3fab27e86940bc543868ca63cbeba451c46
hoangmo998 <codaimohoang@gmail.com>
2021-04-12 23:25:44 +0700

    update

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ColorBox from './components/ColorBox';
+import './App.scss';
+
+function App() {
+    return ( <
+        div className = "app" >
+        <
+        h1 > wklnakl < /h1>  <
+        ColorBox / > < /
+        div >
+    );
+}
+
+export default App;
